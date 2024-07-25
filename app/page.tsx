5f0161bfdde248cad0c76cedce56c7760865b50f
josephjackson0811 <joseph.jackson0811@gmail.com>
2024-07-24 22:49:23 -0400

    complete top section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <>Hello World!</>;
+  return (
+    <main>
+      <div className="flex justify-center items-center h-screen">
+        <div className=" absolute top-[52.5px] left-50% border flex rounded-[500px] h-[88px]">
+          <div className="w-[176px] flex justify-center items-center">
+            image
+          </div>
+          <div className="w-[176px] flex justify-center items-center border-l">
+            image
+          </div>
+        </div>
+        <div className="text-center max-w-[730px]">
+          <div className="text-[64px] text-[#294F74] font-semibold">
+            Your Ultimate Resource Hub for Digital Success
+          </div>
+
+          <div className="text-2xl">
+            Explore our comprehensive library of tools and templates for
+            developers, designers, marketers, and branding experts.
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
