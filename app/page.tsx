7c074343a11c4fdc301b232be7bbff892798a6b0
josephjackson0811 <joseph.jackson0811@gmail.com>
2024-07-25 08:26:42 -0400

    complete middle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import { FilterAltOutlined, Search } from "@mui/icons-material";
-import { Button, IconButton, InputBase, Paper } from "@mui/material";
+import {
+  Code,
+  FilterAltOutlined,
+  InsertDriveFile,
+  Link,
+  Search,
+} from "@mui/icons-material";
+import {
+  Button,
+  Chip,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+} from "@mui/material";
+import Image from "next/image";
 
 const CustomizedInputBase = () => {
   return (
@@ -56,11 +70,11 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="mx-[98px]">
+      <div className="mx-[98px] mb-10">
         <div className=" relative mb-8">
           <CustomizedInputBase />
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between mb-12">
           <div className="flex justify-between gap-4">
             {buttonLables.map((label, index) => (
               <Button
@@ -78,6 +92,169 @@ export default function Home() {
               Filters
             </Button>
           </div>
+        </div>
+        <Grid container spacing={2} className="">
+          <Grid item xs={4} className="flex flex-col gap-4">
+            <div>
+              <Image
+                src={"/1.png"}
+                alt={"1"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              Case Study Template: FlipX, an Investment Attraction Startup
+            </div>
+            <div>
+              <Chip
+                label="Design file"
+                icon={<Link />}
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="flex flex-col gap-4">
+            <div>
+              <Image
+                src={"/2.png"}
+                alt={"2"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              Designing Impression: The Power of First Impressions
+            </div>
+            <div>
+              <Chip
+                label="Github Resource"
+                icon={<Code />}
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="flex flex-col gap-4 mb-20">
+            <div>
+              <Image
+                src={"/3.png"}
+                alt={"3"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              Fluid Card Animation in After Effects
+            </div>
+            <div>
+              <Chip
+                label="Design"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+              <Chip
+                label="UI UX"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+              <Chip
+                label="Branding"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="flex flex-col gap-4">
+            <div>
+              <Image
+                src={"/4.png"}
+                alt={"4"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              Designing Impression: The Power of First Impressions
+            </div>
+            <div>
+              <Chip
+                label="Design"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+              <Chip
+                label="UI UX"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+              <Chip
+                label="Branding"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="flex flex-col gap-4">
+            <div>
+              <Image
+                src={"/5.png"}
+                alt={"5"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              Designing Impression: The Power of First Impressions
+            </div>
+            <div>
+              <Chip
+                label="Design"
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="flex flex-col gap-4">
+            <div>
+              <Image
+                src={"/6.png"}
+                alt={"6"}
+                width={100}
+                height={100}
+                className="w-full rounded-2xl inline-block"
+              />
+            </div>
+            <div className="text-2xl text-[#344054] max-w-[360px]">
+              OpenCore Customer Acquisition Strategy
+            </div>
+            <div>
+              <Chip
+                label="Marketing PDF"
+                icon={<InsertDriveFile />}
+                className="text-[14px] py-1 px-2 rounded-md mr-2"
+              ></Chip>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="w-full px-[74px] py-24">
+        <div className="w-full px-24 py-32 bg-gradient-to-r from-[#2F5478] to-[#22405C] text-white">
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <div className=" max-w-[560px]">
+                <div className=" font-normal text-[64px]">
+                  Transforming your Ideas into reality
+                </div>
+                <div className="font-medium text-xl">
+                  Let's build something extraordinary together to captivate your
+                  audience.
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={6} className="flex justify-end">
+              <div className="rounded-full border w-[250px] h-[250px] flex justify-center items-center">
+                Let’s Work Together!
+              </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
     </main>
